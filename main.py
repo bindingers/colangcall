85b28c3092eb1c83b6b3c493c3f8ffac40deb188
bindingers <93454248+bindingers@users.noreply.github.com>
2021-10-31 05:57:53 +0700

    Adding Echo Function
    Python n PHP Extended

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ $password = trim(fgets(STDIN));
 echo "List Voucher : ";
 $xyz = trim(fgets(STDIN));
 echo "\n";
+echo "\n";
+foreach (explode("\n", str_replace("\r", "", file_get_contents($xyz))) as $key => $akun) {
+    $pecah = explode("|", trim($akun));
+    $voucher = trim($pecah[0]);
+$headers = array();
