249060947afab29f408b57e19f4491af87b42c0c
bindingers <93454248+bindingers@users.noreply.github.com>
2021-10-31 05:59:27 +0700

    dding API Config
    API Extended

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ foreach (explode("\n", str_replace("\r", "", file_get_contents($xyz))) as $key =
     $pecah = explode("|", trim($akun));
     $voucher = trim($pecah[0]);
 $headers = array();
+$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0';
+$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+$gas = curl('API', 'loginFail=0&userid='.$username.'&password='.$password.'', $headers);
